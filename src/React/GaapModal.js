36d25d9dfd549383e7c65d11ec4dd196cdd094a2
loger-kcj <kcj@log-er.com>
2021-01-03 17:50:48 +0900

    모달구현중

---
+++ src/React/GaapModal.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import '../App.css';
+import image0 from "../Image/gaap/0.jpg";
+import image1 from "../Image/gaap/1.jpg";
+import image2 from "../Image/gaap/2.jpg";
+import image3 from "../Image/gaap/3.jpg";
+import image4 from "../Image/gaap/4.jpg";
+import image5 from "../Image/gaap/5.jpg";
+import image6 from "../Image/gaap/6.jpg";
+import image7 from "../Image/gaap/7.jpg";
+import image8 from "../Image/gaap/8.jpg";
+import image9 from "../Image/gaap/9.jpg";
+
+
+const GaapModal = () => {
+
+    return <div>
+        <img className='Modal-Image' src={image0} />
+    </div>
+}
+
+export default GaapModal;
--- src/React/Project.js
+++ src/React/Project.js
@@ -1,14 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import gaap_Logo from "../Image/gaap_Splash.png";
 import '../App.css';
 
+import GaapModal from "./GaapModal";
+
+import GD from "../Image/GD.png";
+
+import QR_B from "../Image/QR_B.png";
+
+import X from "../Image/X.png";
 const Project = () => {
+    const [modalVisible, setModalVisible] = useState(false);
 
+    const SwitchVisible = () => {
+        setModalVisible(!modalVisible)
+    }
 
-    return <div className='Intro'>
+    const clickEvent = ({ click }) => {
+        console.log(click)
+    }
+
+    return <div className='Project'>
         <div>
-            <p className='Intro-Title'>PROJECT</p>
+            <p className='Project-Title'>PROJECT</p>
         </div>
+        {modalVisible ? <div className='Modal' onClick={() => { SwitchVisible(); }}>
+            <div className='Modal-False'> <img className='X_btn' src={X} alt='x' /></div>
+            <div className='Modal-Inner'>
+                <p>소개</p>
+                <div>
+                    <GaapModal />
+                </div>
+            </div>
+        </div> : null}
+        <div className='Project-List'>
+            <div className='Project-Details' onClick={() => { clickEvent({ click: 'gaap' }); SwitchVisible(); }}>
+                <img className='App-Logo' src={gaap_Logo} alt='gaap' />
+                <div className='App-Text'>
+                    <b>Gaap</b>
+                    <p>Flutter로 League of Legends 전적 검색 및 게임중인 소환사 비교
+                        <br />의사소통을 위한 게시판
+                        <br />Firebase를 이용하여 Google Sign in과  League of Legends api에서 받는 소환사명을 연동하여 로그인
+                        <br />Google Admob를 이용한 광고
+                    </p>
+                </div>
+            </div>
 
+            <div className='Project-Details' onClick={() => { clickEvent({ click: 'GD' }) }}>
+                <img className='App-Logo' src={GD} alt='배달앱' />
+                <div className='App-Text'>
+                    <b>광주 배달앱</b>
+                    <p>React Native로 목업용 앱 제작
+                    <br />매장에서 음식 장바구니에 담기까지 개발
+                    <br />&nbsp;
+                    <br />&nbsp;
+                    </p>
+                </div>
+            </div>
+            <div className='Project-Details' onClick={() => { clickEvent({ click: 'gaap' }) }}>
+                <img className='App-Logo' src={QR_B} alt='QR_B' />
+                <div className='App-Text'>
+                    <b>큐아리</b>
+                    <p>React Native로 사업자용 개인용 앱 제작
+                        <br />개인용 앱으로 QR코드를 인식 하여 사업자용 앱에서 확인
+                        <br />개인의 방문기록을 확인 및 삭제 가능
+                        <br />시업자용 앱에서 실시간 입장기록 확인
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 }
 
